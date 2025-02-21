19090d1b9b504ba7aaa3efec252d57c7750ec781
Davide Ravasi <info@davideravasi.com>
2025-02-21 11:26:57 +0100

    feat: added migrations and seed db with data

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -15,7 +15,7 @@ namespace Domain
         public bool IsCancelled { get; set; }
         public required string City { get; set; }
         public required string Venue { get; set; }
-        public double Latituted { get; set; }
+        public double Latitude { get; set; }
         public double Longitude { get; set; }
     }
 }
---
+++ Persistence/Migrations/20250221101548_InitialCreate2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latituted",
+                table: "Activities",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Activities",
+                newName: "Latituted");
+        }
+    }
+}
