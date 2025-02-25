5cc25603ed67eaf09428c0dc28ebf380a558de94
Davide Ravasi <info@davideravasi.com>
2025-02-25 10:06:26 +0100

    feat: added better comments

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(string id)
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
             var activity = await context.Activities.FindAsync(id);
             if (activity == null) return NotFound();
@@ -23,6 +23,11 @@ namespace API.Controllers
         }
     }
 }
+
+// findAsync is not guaranteed to return a non-null value, so we need to check for null and return NotFound if it is null
+// findAsync is a method that is available on DbSet, which is a property of the AppDbContext class
+// DbSet is a collection of entities that can be queried, added, modified, and deleted
+// DbContext is a class that represents a session with the database and allows us to query and save instances of entities
 
 // https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/primary-constructors
 // this class use the Primary constructors paradigm to 
