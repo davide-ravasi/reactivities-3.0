52aff5ef29497e9303a4b43eec604b1e475ef4f1
Davide Ravasi <info@davideravasi.com>
2025-03-04 14:57:26 +0100

    feat: added createActivity handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Command;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -18,6 +19,12 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
             return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command { activity = activity });
         }
     }
 }
