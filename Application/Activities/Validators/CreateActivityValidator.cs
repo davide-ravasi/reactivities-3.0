325cc6c52d7243b8e0cfbb0321e14288a2d45b12
Davide Ravasi <info@davideravasi.com>
2025-07-01 09:29:31 +0200

    feat: implemented rules for activity validation

--- Application/Activities/Validators/CreateActivityValidator.cs
+++ Application/Activities/Validators/CreateActivityValidator.cs
@@ -14,28 +14,25 @@ public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
         RuleFor(x => x.ActivityDto.Description)
             .NotEmpty().WithMessage("Description is required")
             .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
-
-        /*RuleFor(x => x.ActivityDto.Category)
-            .NotEmpty().WithMessage("Category is required")
-            .MaximumLength(50).WithMessage("Category cannot exceed 50 characters");
-
-        RuleFor(x => x.ActivityDto.City)
-            .NotEmpty().WithMessage("City is required")
-            .MaximumLength(100).WithMessage("City cannot exceed 100 characters");
-
-        RuleFor(x => x.ActivityDto.Venue)
-            .NotEmpty().WithMessage("Venue is required")
-            .MaximumLength(200).WithMessage("Venue cannot exceed 200 characters");
-
         RuleFor(x => x.ActivityDto.Date)
             .NotEmpty().WithMessage("Date is required")
-            .GreaterThan(DateTime.Now).WithMessage("Date must be in the future");
+            .GreaterThan(DateTime.UtcNow).WithMessage("Date must be in the future");
+
+        RuleFor(x => x.ActivityDto.Category)
+            .NotEmpty().WithMessage("Category is required");
+
+        RuleFor(x => x.ActivityDto.City)
+            .NotEmpty().WithMessage("City is required");
+
+        RuleFor(x => x.ActivityDto.Venue)
+            .NotEmpty().WithMessage("Venue is required");
 
         RuleFor(x => x.ActivityDto.Latitude)
+            .NotEmpty().WithMessage("Latitude is required")
             .InclusiveBetween(-90, 90).WithMessage("Latitude must be between -90 and 90");
 
         RuleFor(x => x.ActivityDto.Longitude)
+            .NotEmpty().WithMessage("Longitude is required")
             .InclusiveBetween(-180, 180).WithMessage("Longitude must be between -180 and 180");
-        */
     }
 }
